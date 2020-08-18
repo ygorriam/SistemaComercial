167a34f2dd4632ad29d3c3161c2f6a74c68662fb
ygor riam <ygoriam@hotmail.com>
2020-08-18 13:26:01 -0300

    Atualização no controle de cidades.

--- src/controle/ControleCidade.java
+++ src/controle/ControleCidade.java
@@ -5,6 +5,9 @@
  */
 package controle;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 import modelo.ModeloCidade;
 
 /**
@@ -12,7 +15,19 @@ import modelo.ModeloCidade;
  * @author ygori
  */
 public class ControleCidade {
+        ConectaBanco connCidade = new ConectaBanco();
+    
     public void InserirCidade(ModeloCidade mod){
+       connCidade.conexao();
+            try {
+                PreparedStatement pst = connCidade.conn.prepareStatement("insert into cidade(nome_cidades,id_estado)values(?,?)");
+                pst.setString(1,mod.getNome());
+                pst.setInt(2,mod.getCod_estado());
+                pst.execute();
+                JOptionPane.showMessageDialog(null,"Dados armazenados com sucesso");
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null,"Erro na inserção. \n ERRO:"+ex);
+            }
         
     }
     
