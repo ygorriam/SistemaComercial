2af5dea7a78f29d51440f1272dfee766f8954639
ygor riam <ygoriam@hotmail.com>
2020-09-01 14:11:08 -0300

    Modelo do cadastro de bairro.

---
+++ src/modelo/ModeloBairro.java
@@ -0,0 +1,52 @@
+package modelo;
+
+
+public class ModeloBairro {
+    private int cod;
+    private String nome;
+    private String cidade;
+
+    
+    
+    
+    public String getCidade() {
+        return cidade;
+    }
+
+    /**
+     * @param cidade the cidade to set
+     */
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the cod
+     */
+    public int getCod() {
+        return cod;
+    }
+
+    /**
+     * @param cod the cod to set
+     */
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+}
+   
